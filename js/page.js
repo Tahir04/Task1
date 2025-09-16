8a42fdbbd92684b264504da23c66add6823abc23
Tahir Novruzov <fazil.novruzov72@gmail.com>
2025-09-16 14:09:00 +0400

    Header-Final

--- js/page.js
+++ js/page.js
@@ -1,3 +1,4 @@
+// Serach Menu
 let searchMenu = document.getElementById("hd-search-menu");
 let searchDropdown = document.getElementById("search-dropdown");
 let srcMenuOpenButton = document.getElementById("src-menu-open-btn");
@@ -11,5 +12,26 @@ srcMenuOpenButton.addEventListener('click', function(){
     } else{
         searchDropdown.classList.remove("fa-caret-up");
         searchDropdown.classList.add("fa-caret-down");
+    }
+});
+
+
+// --------------------------------------------------------------
+
+// Mobile Category
+
+let categoryMobil = document.getElementById("cat-mbl");
+let categoryDropdown = document.getElementById("cat-dropdown");
+let catMblButton = document.getElementById("cat-mbl-btn");
+
+catMblButton.addEventListener('click', function(){
+    categoryMobil.classList.toggle("search-active");
+
+    if(categoryDropdown.classList.contains("fa-caret-down")){
+        categoryDropdown.classList.remove("fa-caret-down");
+        categoryDropdown.classList.add("fa-caret-up");
+    } else{
+        categoryDropdown.classList.remove("fa-caret-up");
+        categoryDropdown.classList.add("fa-caret-down");
     }
 });
